88c096ca6697af93a553332bef626011b451c214
VladShevchenko9 <vlad9031999@gmail.com>
2024-02-08 21:33:02 +0200

    Transformer bug fix

--- src/ResponseTransformers/AbstractTransformer.ts
+++ src/ResponseTransformers/AbstractTransformer.ts
@@ -3,9 +3,7 @@ import { AbstractModel } from '../Models/AbstractModel'
 export abstract class AbstractTransformer {
     public transform(modelData: AbstractModel | AbstractModel[]): Record<string, any> | Record<string, any>[] {
         if (Array.isArray(modelData)) {
-            modelData.map(model => model.toJson());
-
-            return modelData;
+            return modelData.map(model => this.transform(model));
         }
 
         return modelData.toJson();
